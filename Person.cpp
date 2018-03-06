e022523a1ff940f2f8b144f55895b60f60a5d225
aleksandrabedynska <aleksandrabedynska95@gmail.com>
2018-03-06 23:10:42 +0100

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -18,8 +18,23 @@ void Person::setPseudo()
 	cin >> pseudo;
 }
 
+void Person::setOutput()
+{
+	output = name + "\"" + pseudo + "\"" + surname;
+}
+
 void Person::getOutput()
 {
-	output = name + "\""+ pseudo + "\"" +  surname;
 	cout << output;
+}
+
+void Person::changeLetter()
+{
+	char letter;
+	for (int i = 0; i < output.size(); i++)
+	{
+		letter = output[i];
+		if (letter == 'a' || letter == 'A' || letter == 'e' || letter == 'E' || letter == 'i' || letter == 'I' || letter == 'o' || letter == 'O' || letter == 'y' || letter == 'Y')
+			output[i] = 'z';
+	}
 }
